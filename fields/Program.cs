aa06291c9176cfcec5d02ddbbf46239fe6d2eff9
Robin Lofthus <robin.lofthus@gmail.com>
2021-12-15 12:12:48 +0100

    .

--- fields/Program.cs
+++ fields/Program.cs
@@ -10,7 +10,11 @@ namespace fields
     {
         static void Main(string[] args) 
         {
-            
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            System.Console.WriteLine(customer.Orders.Count);
         }
     }
 }
