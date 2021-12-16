4e9279efcdcabd12ce0f63d6968b643e9f9f61e9
insept <robin.lofthus@gmail.com>
2021-12-17 00:11:26 +0100

    .

---
+++ properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class Person 
+    {
+        public DateTime Birthdate { get; set; }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
