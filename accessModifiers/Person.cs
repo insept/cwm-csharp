b522f88c565ee4f4dc94a7fb255b5b011cc5f7f7
insept <robin.lofthus@gmail.com>
2021-12-16 23:32:22 +0100

    .

---
+++ accessModifiers/Person.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace accessModifiers {
+    public class Person 
+    {
+        private DateTime _birthdate;
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }   
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+}
