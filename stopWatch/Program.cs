ecc1fe564f2b2577e589d9176a30916329bcea6d
insept <robin.lofthus@gmail.com>
2022-01-17 14:14:51 +0100

    .

---
+++ stopWatch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StopWatchExercise // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true) {
+                Console.Write("##### To start and stop the stopwatch, simply type \"start\" to start and \"stop\" to stop and then press enter ##### \n: ");
+                var consoleInupt = Console.ReadLine();
+
+                if (consoleInupt == "start" || consoleInupt == "stop") 
+                {
+                    var stopWatch = new StopWatch();
+                    if (consoleInupt == "start") 
+                    {
+                        stopWatch.StartTimer(DateTime.Now);
+                    }
+                    if (consoleInupt == "stop") 
+                    {
+                        stopWatch.StopTimer(DateTime.Now);
+
+                        System.Console.WriteLine("The duration was: " + stopWatch.Duration());
+                    }
+
+                }
+                if (consoleInupt == "exit" || consoleInupt == "quit")
+                {
+                    System.Console.WriteLine("Exiting application...");
+                    break;
+                }
+                else
+                {
+                    System.Console.WriteLine("Invalid input...READ THE INSTRUCTIONS YOU FUCKING CUNT!"); 
+                }
+            }
+        }
+    }
+}
---
+++ stopWatch/StopWatch.cs
@@ -0,0 +1,29 @@
+namespace StopWatchExercise // Note: actual namespace depends on the project name.
+{
+    public class StopWatch {
+        public DateTime StartTimer(DateTime start)
+        {
+           StartTime = start;
+
+           return StartTime;
+        }
+        public DateTime StartTime {get; private set;}
+
+        public DateTime StopTimer(DateTime stop) 
+        {
+            StopTime = stop;
+
+            return StopTime;
+        }
+        public DateTime StopTime {get; private set;}
+
+        public TimeSpan Duration()
+        {
+            var TimeDuration = StartTime - StopTime;
+
+            return TimeDuration;
+        }
+        public TimeSpan TimeDuration {get; private set;}
+
+    }
+}
